e1d175ae4ff48333145dd32e59ac7e2a91fffecc
rafet <83957086+Rafetdur@users.noreply.github.com>
2022-01-29 12:17:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+//reusability
+    public static void main(String[] args) {
+        int ogrenciSayi = 11;
+System.out.println("Ögrenci Sayım: "+ogrenciSayi);
+String mesaj = "Ögrenci Sayısı: ";
+        System.out.println(mesaj + ogrenciSayi);
+        System.out.println(mesaj + ogrenciSayi);
+        System.out.println(mesaj + ogrenciSayi);
+        System.out.println(mesaj + ogrenciSayi);
+        System.out.println(mesaj + ogrenciSayi);
+    }
+}
